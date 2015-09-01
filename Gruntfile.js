279f80bb2bb4d9393808082b1d1f36eba28a3bef
Alex Anderson <anderson@medicmobile.org>
2015-09-01 15:06:27 +0200

    Separate xpath evaluator binding into separate module
    This should make it simpler to stay in sync with `master`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,6 +114,7 @@ module.exports = function( grunt ) {
             options: {
                 alias: {
                     jquery:'./jquery-shim.js',
+                    'xpath-evaluator-binding':'./src/js/OpenrosaXpathEvaluatorBinding.js',
                 },
             },
         },
